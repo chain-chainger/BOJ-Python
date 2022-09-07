5ad751596578ff0c9a1487bd4cf56a3e00da5b53
chain.chainger <chain.chainger@gmail.com>
2022-09-07 10:04:44 +0900

    22.09.07

--- 문자열_1120.py
+++ 문자열_1120.py
@@ -3,7 +3,7 @@ min = 50
 for i in range(len(b) - len(a) + 1):
     cnt = 0
     for j in range(len(a)):
-        if a[j] != b[i + j]:
+        if a[j] != b[i+j]:
             cnt += 1
     if cnt < min:
         min = cnt
