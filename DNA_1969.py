9312521f07d77c08eb527ae7d4606ce3ad49a3b4
chain.chainger <chain.chainger@gmail.com>
2022-09-11 21:18:08 +0900

    22.09.11

---
+++ DNA_1969.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr = []
+res = ""
+cnt = 0
+for i in range(n):
+    arr.append(list(map(str, input())))
+for i in range(m):
+    a = [0 for i in range(26)]
+    for j in range(n):
+        a[ord(arr[j][i]) - 65] += 1
+    res += chr(a.index(max(a)) + 65)
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] != res[j]:
+            cnt += 1
+print(f"{res}\n{cnt}")
