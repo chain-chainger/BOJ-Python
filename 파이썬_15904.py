305b85f53104ae2ed7c60082641ee1889383eb3a
chain.chainger <chain.chainger@gmail.com>
2022-10-09 16:31:20 +0900

    22.10.09

---
+++ 파이썬_15904.py
@@ -0,0 +1,15 @@
+s = input()
+ucpc = "UCPC"
+i = 0
+flag = False
+for c in s:
+    if c == ucpc[i]:
+        i += 1
+    if i == 4:
+        flag = True
+        break
+if flag:
+    print("I love UCPC")
+else:
+    print("I hate UCPC")
+       
