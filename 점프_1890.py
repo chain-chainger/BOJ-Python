7ef864ba27379ac691ff1280764fb2d6e7bdc5ad
chain.chainger <chain.chainger@gmail.com>
2022-12-19 09:18:34 +0900

    22.12.19

---
+++ 점프_1890.py
@@ -0,0 +1,12 @@
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
+for i in range(n):
+    for j in range(n):
+        if i == n - 1 and j == n - 1:
+            print(dp[i][j])
+        if i + board[i][j] < n:
+            dp[i + board[i][j]][j] += dp[i][j]
+        if j + board[i][j] < n:
+            dp[i][j + board[i][j]] += dp[i][j]
