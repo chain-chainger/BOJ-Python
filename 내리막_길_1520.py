2a38f20a5611a1ac7ff2ee173ef806a349753316
chain-chainger <chain.chainger@gmail.com>
2022-12-20 20:19:01 +0900

    22.12.20

---
+++ 내리막_길_1520.py
@@ -0,0 +1,18 @@
+def dfs(x, y):
+	if x == m - 1 and y == n - 1:
+		return 1
+	if dp[x][y] != -1:
+		return dp[x][y]
+	dp[x][y] = 0
+	for i in range(4):
+		nx, ny = x + dx[i], y + dy[i]
+		if 0 <= nx < m and 0 <= ny < n:
+			if map[x][y] > map[nx][ny]:
+				dp[x][y] += dfs(nx, ny)
+	return dp[x][y]
+
+m, n = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+print(dfs(0, 0))
