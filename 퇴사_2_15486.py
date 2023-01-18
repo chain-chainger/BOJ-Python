1ea00afc9a947d2ba08608559e14c182a5f0a656
chain.chainger <chain.chainger@gmail.com>
2023-01-18 13:06:20 +0900

    23.01.18

---
+++ 퇴사_2_15486.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+t = []
+p = []
+dp = [0] * (n + 1)
+for _ in range(n):
+    a, b = map(int, input().split())
+    t.append(a)
+    p.append(b)
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(p[i] + dp[i+t[i]], dp[i+1])
+print(dp[0])
