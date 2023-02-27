d7591e690210b97abf2a2ce89da9a608a588d60b
chain.chainger <chain.chainger@gmail.com>
2023-02-27 20:24:22 +0900

    23.02.27

---
+++ 미로_탐색_2178.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(x, y):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if maze[nx][ny] == 0:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx, ny))
+    return maze[n - 1][m - 1]
+n, m = map(int, input().split())
+maze = [list(map(int, input().rstrip())) for _ in range(n)]
+print(bfs(0, 0))
