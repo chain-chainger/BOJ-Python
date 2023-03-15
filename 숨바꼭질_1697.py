7f8c61689c0f3951d2b7808ff4bddaea4ccb2e1c
chain.chainger <chain.chainger@gmail.com>
2023-03-15 23:09:25 +0900

    23.03.15

--- 숨바꼭질_1697.py
+++ 숨바꼭질_1697.py
@@ -7,7 +7,7 @@ def bfs():
     while queue:
         x = queue.popleft()
         for nx in (x - 1, x + 1, x * 2):
-            if 0 <= nx < MAX and visited[nx] == 0:
+            if 0 <= nx <= MAX and visited[nx] == 0:
                 visited[nx] = visited[x] + 1
                 if nx == k:
                     return visited[nx]
