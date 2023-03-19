90aa824ebe2562a513c71f9296b1ba36cd2b7452
chain.chainger <chain.chainger@gmail.com>
2023-03-19 22:09:59 +0900

    23.03.19

---
+++ N과_M_(5)_15655.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(start):
+    if len(sequence) == m:
+        print(*sequence)
+        return
+    for number in n_list:
+        if number in sequence or number < start:
+            continue
+        sequence.append(number)
+        dfs(number)
+        sequence.pop()
+
+n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
+n_list.sort()
+sequence = []
+dfs(n_list[0])
