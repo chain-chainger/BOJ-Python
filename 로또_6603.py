11b0a6c00fc83c6725280cb6c80e286dfb3286d3
chain-chainger <chain.chainger@gmail.com>
2023-03-25 16:28:09 +0900

    23.03.25

---
+++ 로또_6603.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(index):
+    if len(sequence) == 6:
+        print(*sequence)
+        return
+    for i in range(index, k):
+        sequence.append(s[i])
+        dfs(i + 1)
+        sequence.pop()
+
+while True:
+    temp = list(map(int, input().split()))
+    k = temp[0]
+    if k == 0:
+        break
+    s = temp[1:]
+    sequence = []
+    dfs(0)
+    
