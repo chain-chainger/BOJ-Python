a621422eef205ce1e431f182036dbdecd45f33f9
chain.chainger <chain.chainger@gmail.com>
2023-03-31 09:30:49 +0900

    23.03.31

---
+++ 쌍의_합_5217.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    print(f"Pairs for {n}:", end=' ')
+    for i in range(1, (n // 2) + 1):
+        if i != n - i:
+            if i != 1:
+                print(',', end=' ')
+            print(i, n - i, end='')
+    print()
