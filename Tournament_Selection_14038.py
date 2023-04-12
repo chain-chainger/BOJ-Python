caaa86aa7808ee190684cb3080853fcd28ef58e1
chain.chainger <chain.chainger@gmail.com>
2023-04-12 22:24:42 +0900

    23.04.12

---
+++ Tournament_Selection_14038.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+count = 0
+for _ in range(6):
+    if input().rstrip() == 'W':
+        count += 1
+if count >= 5:
+    print(1)
+elif count >= 3:
+    print(2)
+elif count >= 1:
+    print(3)
+else:
+    print(-1)
