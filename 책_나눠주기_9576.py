5d0a593c4558d71bf9c26f8e74c11e3813445560
chain-chainger <chain.chainger@gmail.com>
2023-04-25 20:45:56 +0900

    23.04.25

--- 책_나눠주기_9576.py
+++ 책_나눠주기_9576.py
@@ -1,5 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n1, k1, n2, k2 = map(int, input().split())
-print((n1 * k1) + (n2 * k2))
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    books = [False] * (n + 1)
+    requests = [list(map(int, input().split())) for _ in range(m)]
+    requests.sort(key = lambda x:x[1])
+    count = 0
+    for a, b in requests:
+        for i in range(a, b + 1):
+            if books[i] == False:
+                books[i] = True
+                count += 1
+                break
+    print(count)
