472c2fc9c762eaec1a1938be3771a139cc366af9
chain.chainger <chain.chainger@gmail.com>
2023-06-05 09:22:15 +0900

    23.06.05

---
+++ 좋은수열_2661.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def recursive(index):
+    for i in range(1, (index // 2) + 1):
+        if sequence[-i:] == sequence[-2 * i:-i]:
+            return -1
+    if index == n:
+        for i in range(n):
+            print(sequence[i], end='')
+        return 0
+    for i in range(1, 4):
+        sequence.append(i)
+        if recursive(index + 1) == 0:
+            return 0
+        sequence.pop()
+n = int(input())
+sequence = []
+recursive(1)
