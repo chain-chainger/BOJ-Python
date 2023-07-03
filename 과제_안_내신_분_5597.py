63bad97fd6d11cb3dbf41b93fd149e7c8ec16000
chain.chainger <chain.chainger@gmail.com>
2023-07-03 22:50:29 +0900

    23.07.03

---
+++ 과제_안_내신_분_5597.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+students = [_ for _ in range(1, 31)]
+for _ in range(28):
+    temp = int(input())
+    students.remove(temp)
+print(min(students))
+print(max(students))s
