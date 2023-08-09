116493e16165311b2e6793882fc78cd078713f95
chain.chainger <chain.chainger@gmail.com>
2023-08-09 23:58:19 +0900

    23.08.09

---
+++ 직각_삼각형의_두_변_6322.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+count = 1
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        break
+    if count > 1:
+        print()
+    print(f"Triangle #{count}")
+    if c == -1:
+        print("c = %.3f" % ((a ** 2 + b ** 2) ** 0.5))
+    elif max(a, b) >= c:
+        print("Impossible.")
+    elif a == -1:
+        print("a = %.3f" % ((c ** 2 - b ** 2) ** 0.5))
+    elif b == -1:
+        print("b = %.3f" % ((c ** 2 - a ** 2) ** 0.5))    
+    count += 1
