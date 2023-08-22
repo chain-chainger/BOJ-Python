c175517213974a454f69fb129c25ec91711c8fd9
chain.chainger <chain.chainger@gmail.com>
2023-08-22 14:44:39 +0900

    23.08.22

---
+++ 찍기_2966.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+answers = input().rstrip()
+adrian = ['A', 'B', 'C']
+bruno = ['B', 'A', 'B', 'C']
+goran = ['C', 'C', 'A', 'A', 'B', 'B']
+adrian_count = bruno_count = goran_count = 0
+for i in range(len(answers)):
+    if adrian[i % 3] == answers[i]:
+        adrian_count += 1
+    if bruno[i % 4] == answers[i]:
+        bruno_count += 1
+    if goran[i % 6] == answers[i]:
+        goran_count += 1
+max_count = max(adrian_count, bruno_count, goran_count)
+print(max_count)
+if max_count == adrian_count:
+    print("Adrian")
+if max_count == bruno_count:
+    print("Bruno")
+if max_count == goran_count:
+    print("Goran")
