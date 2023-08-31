14a7a5b61c7e6cc49c8a7dafa8a3c286b3c3b53d
chain.chainger <chain.chainger@gmail.com>
2023-08-31 14:03:02 +0900

    23.08.31

---
+++ 바구니_뒤집기_10811.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+baskets = [i for i in range(1, n + 1)]
+for _ in range(m):
+    i, j = map(int, input().split())
+    temp = baskets[i - 1 : j]
+    temp.reverse()
+    baskets[i - 1 : j] = temp
+print(*baskets)
