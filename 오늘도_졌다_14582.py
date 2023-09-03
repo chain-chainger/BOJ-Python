50f0e930cce7c97cbab538580a08d02eb6ecba5c
chain-chainger <chain.chainger@gmail.com>
2023-09-03 20:10:53 +0900

    23.09.03

--- 오늘도_졌다_14582.py
+++ 오늘도_졌다_14582.py
@@ -7,7 +7,7 @@ flag = False
 a_sum = b_sum = 0
 for i in range(9):
     a_sum += a[i]
-    b_sum += b[i]
     if a_sum > b_sum:
         flag = True
+    b_sum += b[i]
 print("Yes" if flag else "No")
