a01dc8e070dd27989411208753e465933d8acef0
chain.chainger <chain.chainger@gmail.com>
2023-12-13 23:10:50 +0900

    23.12.13

--- 분산처리_1009.py
+++ 분산처리_1009.py
@@ -2,6 +2,24 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
+
 for _ in range(T):
     a, b = map(int, input().split())
-    print((a ** b) % 10)
+    a = a % 10
+    
+    if a == 0:
+        print(10)
+    elif a in [1, 5, 6]:
+        print(a)
+    elif a in [4, 9]:
+        b = b % 2
+        if b == 0:
+            print((a ** 2) % 10)
+        else:
+            print(a)
+    else:
+        b = b % 4
+        if b == 0:
+            print((a ** 4) % 10)
+        else:
+            print((a ** b) % 10)
