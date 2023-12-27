7682c985e61e5669f5d8e905675237ec25bf8aeb
chain.chainger <chain.chainger@gmail.com>
2023-12-27 22:49:35 +0900

    23.12.27

---
+++ 음계_2920.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+numbers = list(map(int, input().split()))
+
+if numbers == sorted(numbers):
+    print("ascending")
+elif numbers == sorted(numbers, reverse=True):
+    print("descending")
+else:
+    print("mixed")
