e88a76aec23c21c41ead1e51d5f7f54bfb50c35c
chain.chainger <chain.chainger@gmail.com>
2024-01-16 10:43:52 +0900

    24.01.16

---
+++ 집합_11723.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+m = int(input())
+s = set()
+
+for _ in range(m):
+    temp = list(input().rstrip().split())
+    
+    if len(temp) == 1:
+        if temp[0] == "all":
+            s = set([i for i in range(1, 21)])
+        elif temp[0] == "empty":
+            s = set()
+    else:
+        command, target = temp[0], temp[1]
+        taget = int(target)
+        
+        if command == "add":
+            s.add(target)
+        elif command == "remove":
+            s.discard(target)
+        elif command == "check":
+            print(1 if target in s else 0)
+        elif command == "toggle":
+            if target in s:
+                s.remove(target)
+            else:
+                s.add(target)
